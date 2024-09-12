aff7ab74d67686b7642a27c9b23361aa7f16a72b
lukgvo <lukgvo@outlook.com>
2024-09-13 08:46:06 +1000

    Added 'new book' and point system

--- libSys.py
+++ libSys.py
@@ -1,4 +1,4 @@
-books = ["poobook", "book", "the best book", "not good book"]
+books = ["poobook", "book", "the best book", "not good book", "new book"]
 
 print("books available: ")
 for book in books:
@@ -7,7 +7,11 @@ for book in books:
 borrowed_book = input("\n write the name of the book you wish to borrow: ")
 
 if borrowed_book in books:
-    borrow_time = input("how many days to borrow the book for?")
+    borrow_time = int(input("how many days to borrow the book for?")) 
     print(f"\n'{borrowed_book}' has been borrowed for {borrow_time} days")
 else:
     print(f"'{borrowed_book}' is not available")
+
+points = borrow_time * 10 
+
+print(f"\nyou have earnt '{points}' points")
