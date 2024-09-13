dbf70f3d42f6cb11ff1949716535f00252267f65
Sam Mason <s224443645@deakin.edu.au>
2024-09-13 10:02:56 +1000

    Add book class and modified libSys to take advantage of it

--- libSys.py
+++ libSys.py
@@ -1,13 +1,17 @@
+from book import Book
+
 #the array of books
-books = ["poobook", "book", "the best book", "not good book", "new book"]
+# books = ["poobook", "book", "the best book", "not good book", "new book"]
+books = [Book("poobook"), Book("book"), Book("the best book"), Book("not good book"), Book("new book")]
 
 print("books available: ")
 for book in books:
-    print(f"- {book}")
+    print(f"- {book.name}")
 
-borrowed_book = input("\n write the name of the book you wish to borrow: ")
+borrowed_book = input("\n write the name of the book you wish to borrow: ").strip()
 
-if borrowed_book in books:
+
+if borrowed_book in [book.name for book in books]:
     borrow_time = int(input("how many days to borrow the book for?")) 
     print(f"\n'{borrowed_book}' has been borrowed for {borrow_time} days")
 else:
